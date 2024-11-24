5a594d14f6760b8b1764099c3e3384085c51e788
hyunho4532 <kyounguk192@gmail.com>
2024-11-24 18:11:09 +0900

    feat. 스플래시 화면 기능 구현 중

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,6 +1,42 @@
+import { supabase } from '@/shared/util/supabase';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin'
+
 export default function ProfileScreen() {
+
+  GoogleSignin.configure({
+    scopes: ['https://www.googleapis.com/auth/drive.readonly'],
+    webClientId: '770007854771-3tmu39bss76v84g3i04vs2k0erd27jkf.apps.googleusercontent.com'
+  })
+
   return (
     <>
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={async () => {
+          try {
+            await GoogleSignin.hasPlayServices()
+
+            const userInfo = await GoogleSignin.signIn()
+            if (userInfo.data?.idToken) {
+              const { data, error } = await supabase.auth.signInWithIdToken({
+                provider: 'google',
+                token: userInfo.data.idToken
+              })
+
+              console.log(error, data);
+            } else {
+              throw new Error('no ID token present!')
+            }
+          } catch (error: any) {
+            
+          }
+        }}
+      />
     </>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,8 +29,11 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="(splash)" />
+        <Stack.Screen name="(tabs)" />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
