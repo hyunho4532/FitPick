ffd9e7c2a21cdc71e4c40cc848151e513caef115
hyunho4532 <kyounguk192@gmail.com>
2024-11-24 14:55:52 +0900

    feat. supabase 이용하여 로그인 기능 테스트 중

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,21 +1,30 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+import { Button } from 'react-native';
+import * as AuthSession from 'expo-auth-session'
+import { supabase } from '@/shared/util/supabase';
 
 export default function ProfileScreen() {
+
+  const redirectUrl = AuthSession.makeRedirectUri();
+
   return (
     <>
+      <Button title='테스트' onPress={async () => {
+        const { data, error } = await supabase.auth.signInWithOAuth({
+          provider: 'google',
+          options: {
+            redirectTo: redirectUrl,
+            queryParams: {
+              access_type: 'offline',
+              prompt: 'consent',
+            }
+          }
+        })
+
+        if (data) {
+          console.error(data);
+        }
+
+      }} />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
-});
