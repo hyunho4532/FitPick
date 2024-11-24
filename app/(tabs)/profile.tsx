4b2da1076ae5e13bc3f33405f9585eb320e2a15c
hyunho4532 <kyounguk192@gmail.com>
2024-11-24 15:03:07 +0900

    chore. google-sign-in npm add.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,30 +1,6 @@
-import { Button } from 'react-native';
-import * as AuthSession from 'expo-auth-session'
-import { supabase } from '@/shared/util/supabase';
-
 export default function ProfileScreen() {
-
-  const redirectUrl = AuthSession.makeRedirectUri();
-
   return (
     <>
-      <Button title='테스트' onPress={async () => {
-        const { data, error } = await supabase.auth.signInWithOAuth({
-          provider: 'google',
-          options: {
-            redirectTo: redirectUrl,
-            queryParams: {
-              access_type: 'offline',
-              prompt: 'consent',
-            }
-          }
-        })
-
-        if (data) {
-          console.error(data);
-        }
-
-      }} />
     </>
   );
 }
