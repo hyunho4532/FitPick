230f595c1120f0c44f18aaf8745514cbf0948ca8
hyunho4532 <kyounguk192@gmail.com>
2024-11-26 22:10:31 +0900

    feat. images.json add.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -3,8 +3,8 @@ import { CardIsNotLogin, ContainerIsNotLogin, TitleText } from "../styles/child"
 export default function ProfileScreen() {
   return (
     <ContainerIsNotLogin>
-      <CardIsNotLogin>
-        <TitleText>진행하려면 로그인을 진행해주세요!</TitleText>
+      <CardIsNotLogin backgroundColor="#bfd5e2">
+        <TitleText paddingTop="16" color="gray">진행하려면 로그인을 진행해주세요!</TitleText>
       </CardIsNotLogin>
     </ContainerIsNotLogin>
   );
--- app/styles/child/index.ts
+++ app/styles/child/index.ts
@@ -6,10 +6,12 @@ export const ContainerRoot = styled(Container)`
 `
 
 /** 로그인이 진행되지 않았을 때의 Card 속성 */
-export const CardIsNotLogin = styled(Card)`
+export const CardIsNotLogin = styled(Card)<{
+    backgroundColor: string
+}>`
     width: 320px;
     height: 140px;
-    background-color: #FEF9F2;
+    background-color: ${(props) => props.backgroundColor};
     border-radius: 24px;
 `
 
@@ -19,8 +21,21 @@ export const ContainerIsNotLogin = styled(Container)`
     align-items: center;
 `
 
-export const TitleText = styled(Text)`
+/** 콘텐츠에 메인 제목 */
+export const TitleText = styled(Text)<{
+    paddingTop: string,
+    color: string
+}>`
     font-size: 18px;
     font-weight: bold;
     text-align: center;
+    padding-top: ${(props) => props.paddingTop}px;
+    color: ${(props) => props.color};
+`
+
+/** 콘텐츠의 서브 제목 */
+export const SubText = styled(Text)<{
+
+}>`
+
 `
