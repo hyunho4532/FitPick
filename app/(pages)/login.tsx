12c711bf96a35893f0ba591634a9446f47c52903
hyunho4532 <kyounguk192@gmail.com>
2024-11-28 20:37:56 +0900

    feat. images.json redirect field add.

--- app/(pages)/login.tsx
+++ app/(pages)/login.tsx
@@ -1,4 +1,4 @@
-export function AppBar() {
+export function LoginPage() {
     return (
         <>
         </>
---
+++ app/styles/child/field/index.ts
@@ -0,0 +1,8 @@
+import styled from '@emotion/native'
+
+/**
+ * 모든 Field를 여기에 관리한다.
+ */
+export const TextInput = styled.TextInput`
+
+`
